2472543dd3c99008cd37da4e9e8a00dd957ff386
Carlos Franzreb <carlosfranzreb@gmail.com>
2020-01-22 21:26:53 +0100

    Remove subrecipes section.

--- syp/static/js/edit_recipe.js
+++ syp/static/js/edit_recipe.js
@@ -15,37 +15,7 @@ $(document).ready(function() {
       ingredient.val(ing_val.substring(12, ing_val.length - 2));
     });
   }
-  // Update subrecipe options for subrecipes
-  $("#subrecipes li input").each(function() {
-    $("#all_subrecipes option[value='" + $(this).val() + "']").remove();
-  });
-  // Update subrecipe options for the steps
-  var select = $("#choose_subrecipe");
-  var all_subrecipes = [];
-  $("#subrecipes ul li").each(function() {
-    all_subrecipes.push(
-      $(this)
-        .find("input")
-        .val()
-    );
-  });
-  var used_subrecipes = [];
-  $("#steps ol li textarea")
-    .filter(".is_subrecipe")
-    .each(function() {
-      used_subrecipes.push($(this).val());
-    });
-  var subrecipes = all_subrecipes.filter(x => !used_subrecipes.includes(x));
-  for (idx = 0; idx < subrecipes.length; idx++) {
-    select.append(new Option(subrecipes[idx], subrecipes[idx]));
-  }
-  select.val(
-    select
-      .children("option")
-      .first()
-      .val()
-  );
-  update_subrecipe_anchor();
+
 
   // Disable submit on enter
   $(window).keydown(function(event){
@@ -97,14 +67,7 @@ function add_item(link) {
   if (n_items == 1 && first_item.css("display") == "none") {
     first_item.css("display", "block");
   } else {
-    var new_item = "";
-    if (div == "subrecipes") {
-      new_item = $("#blueprint_subrecipes")
-        .children(0)
-        .clone();
-    } else {
-      new_item = first_item.clone();
-    }
+    var new_item = first_item.clone();
     // Change values
     new_item.find("input, textarea").each(function() {
       $(this).val("");
@@ -155,20 +118,14 @@ function remove_item(link) {
   }
   var n_items = items.length;
   if (n_items == 1) {
-    if (section == "subrecipes") {
-      $(link)
-        .parent()
-        .remove();
-    } else {
-      i = items.first();
-      i.find("input, textarea").each(function() {
-        $(this).val("");
-      });
-      i.find("select")
-        .first()
-        .val("0");
-      i.css("display", "none");
-    }
+    i = items.first();
+    i.find("input, textarea").each(function() {
+      $(this).val("");
+    });
+    i.find("select")
+      .first()
+      .val("0");
+    i.css("display", "none");
   } else {
     var item_id = $(link)
       .siblings("input, textarea")
@@ -205,115 +162,32 @@ function remove_item(link) {
   }
 }
 
-function add_subrecipe_steps() {
-  $("#choose_subrecipe").css("display", "block");
+function toggle_subrecipe_input() {
+  $("#choose_subrecipe").toggle();
+  $("#subrecipe_button").toggle();
+  $("#anchor_add_step").toggle();
+  let anchor = $("#anchor_add_subrecipe");
+  anchor.text(anchor.text() == "Cancelar" ? "Añadir subreceta" : "Cancelar");
 }
 
-function set_subrecipe_step() {
-  // Subrecipe is added to the steps and removed from the options
-  var select = $("#choose_subrecipe");
-  var added_item = add_item(select.siblings("a:contains(paso)"));
-  select.css("display", "none");
-  var selected_option = select.find("option:selected");
-
-  added_item
-    .find("textarea")
-    .val(selected_option.text())
-    .attr("readonly", true)
-    .addClass("is_subrecipe");
-  added_item
-    .children("a")
-    .attr("onclick", "remove_item(this); update_subrecipe_options(this)");
-  selected_option.remove();
-  select.val(
-    select
-      .children("option")
-      .first()
-      .val()
-  );
-  update_subrecipe_anchor();
-}
-
-function remove_subrecipe(link) {
-  var subrecipe = $(link)
-    .siblings("input")
-    .val();
-  var is_removable = true;
-  $("#steps ol li textarea")
-    .filter(".is_subrecipe")
-    .each(function() {
-      if ($(this).val() == subrecipe) {
-        is_removable = false;
-        alert(
-          "No se puede borrar la subreceta '" +
-            subrecipe +
-            "', porque está en los pasos de la receta."
-        );
-      }
-    });
-  if (is_removable == true) {
-    $("#all_subrecipes").append(new Option("", subrecipe));
-    // Remove subrecipe from step options
-    // $("#choose_subrecipe option[value='" + subrecipe + "']").remove();
-    $("#choose_subrecipe option").each(function() {
-      if ($(this).val() == subrecipe) {
-        $(this).remove();
-      }
-    });
-    update_subrecipe_anchor();
-    remove_item(link);
+function add_subrecipe() {
+  let input = $("#choose_subrecipe");
+  let subrecipe = input.val();
+  let obj = $("#all_subrecipes").find("option[value='" + subrecipe + "']");
+  if(obj != null && obj.length > 0) {
+    let added_item = add_item(input.siblings("a:contains(paso)"));
+    input.val('');  // Remove input.
+    toggle_subrecipe_input();
+    added_item
+      .find("textarea")
+      .val(subrecipe)
+      .attr("readonly", true)
+      .addClass("is_subrecipe")
+      .children("a")
+      .attr("onclick", "remove_item(this)");
   }
-}
-
-function add_subrecipe_to_step_options(link, from_subrecipe = false) {
-  var subrecipe = "";
-  if (from_subrecipe == true) {
-    // Subrecipe was added to subrecipes
-    subrecipe = $(link).val();
-  } else {
-    // Subrecipe was removed from steps
-    subrecipe = $(link)
-      .siblings("textarea")
-      .val();
-  }
-  var select = $("#choose_subrecipe");
-  select.append(new Option(subrecipe, subrecipe));
-  select.val(
-    select
-      .children("option")
-      .first()
-      .val()
-  );
-  update_subrecipe_anchor();
-}
-
-function update_subrecipe_options(item) {
-  var before = $(item).attr("data-before");
-  if (before != "None") {
-    $("#all_subrecipes").append(new Option("", before)); // add_subrecipe_to_subrecipe_options
-    $("#choose_subrecipe option[value='" + before + "']").remove(); // remove_subrecipe_from_step_options
-  }
-  $("#all_subrecipes option[value='" + $(item).val() + "']").remove(); // remove_subrecipe_from_subrecipe_options
-  add_subrecipe_to_step_options(item, (from_subrecipe = true));
-  $(item).attr("data-before", $(item).val()); // Update data-before
-  update_subrecipe_anchor();
-}
-
-function update_subrecipe_anchor() {
-  var select = $("#choose_subrecipe");
-  select.css("display", "none");
-  var link = $("#anchor_add_subrecipe");
-  var options = select.children("option");
-  if (options.length == 1 && link.text() == "Añadir subreceta") {
-    link
-      .text("Subrecetas incluidas")
-      .css("color", "#599A00")
-      .attr("onclick", "");
-  } else if (options.length > 1 && link.text() == "Subrecetas incluidas") {
-    link
-      .text("Añadir subreceta")
-      .css("color", "#F56A6A")
-      .attr("onclick", "add_subrecipe_steps()");
+  else {  // don't allow submission
+    alert("La subreceta que has escrito no existe. Elige una de la lista.");
   }
 }
 
