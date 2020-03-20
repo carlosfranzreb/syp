0bf244428aeb0c43d19917723ca7f4d6e4e0e2ee
Carlos Franzreb <carlosfranzreb@gmail.com>
2020-03-20 10:37:26 +0100

    Bugfixes.

--- syp/models/subrecipe.py
+++ syp/models/subrecipe.py
@@ -4,6 +4,8 @@ It is not a recipe itself; it belongs to a recipe. """
 
 
 from syp import db
+from syp.models.subrecipe_step import SubrecipeStep
+from syp.models.subquantity import Subquantity  # TODO: Why are these imports necessary?
 
 
 class Subrecipe(db.Model):
@@ -11,7 +13,7 @@ class Subrecipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     steps = db.Column(db.Text, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False)
-    case_fem = db.Column(db.Boolean, nullable=False)  # 0=masc., 1=fem.
+    created_at = db.Column(db.DateTime, nullable=False)
+    is_feminine = db.Column(db.Boolean, nullable=False)  # 0=masc., 1=fem.
     steps = db.relationship('SubrecipeStep', backref='subrecipe', lazy=True)
     ingredients = db.relationship('Subquantity', backref='subrecipe', lazy=True)
