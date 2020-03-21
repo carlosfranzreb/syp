e021c6d1c60740bbe90cc3b1c4fbd187529faa7d
Carlos Franzreb <carlosfranzreb@gmail.com>
2020-03-21 10:35:42 +0100

    Adds route to create subrecipe.

--- syp/subrecipes/routes.py
+++ syp/subrecipes/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required
 
-from syp.subrecipes import utils, update
+from syp.subrecipes import utils, update, create, validate
 from syp.search.forms import SearchRecipeForm
 from syp.recipes.utils import get_last_recipes
 from syp.subrecipes.forms import SubrecipeForm
 from syp.models.unit import Unit
+from syp.ingredients.utils import get_all_ingredients
 
 
 subrecipes = Blueprint("subrecipes", __name__)
@@ -34,16 +35,17 @@ def edit_subrecipe(subrecipe_url):
             (u.id, u.singular) for u in Unit.query.order_by(Unit.singular)
         ]
     if form.validate_on_submit():
+        # TODO: Check uniqueness of name, valid ingredients, etc.
         update.update_subrecipe(subrecipe, form)
         flash("Los cambios han sido guardados.", "success")
         return redirect(url_for("subrecipes.overview"))
     return render_template(
         "edit_subrecipe.html",
-        title="Subrecetas",
+        title="Editar subreceta",
         subrecipe=subrecipe,
         recipe_form=SearchRecipeForm(),
+        all_ingredients=get_all_ingredients(),
         last_recipes=get_last_recipes(4),
-        subrecipes=utils.get_paginated_subrecipes()[1],
         form=form,
         is_edit_recipe=True
     )
@@ -51,6 +53,7 @@ def edit_subrecipe(subrecipe_url):
 @subrecipes.route("/borrar_subreceta/<subrecipe_url>")
 @login_required
 def delete_subrecipe(subrecipe_url):
+    # TODO: Window alert before deleting.
     subrecipe = utils.get_subrecipe_by_url(subrecipe_url)
     if subrecipe.uses() > 0:
         flash('La subreceta no se puede borrar. Hay recetas que la usan.', 'danger')
@@ -58,3 +61,36 @@ def delete_subrecipe(subrecipe_url):
         utils.delete_subrecipe(subrecipe)
         flash('La subreceta ha sido borrada.', 'success')
     return redirect(url_for('subrecipes.overview'))
+
+
+@subrecipes.route('/crear_subreceta/<subrecipe_url>', methods=['GET', 'POST'])
+@login_required
+def create_subrecipe(subrecipe_url):
+    subrecipe = utils.create_subrecipe()
+    form = SubrecipeForm(obj=subrecipe)
+    for subform in form.ingredients:
+        subform.unit.choices = [
+        (u.id, u.singular) for u in Unit.query.order_by(Unit.singular)
+    ]
+    if form.validate_on_submit():
+        errors = validate.validate(form)
+        if len(errors) > 0:
+            for error in errors:
+                flash(error, 'danger')
+        else:
+            create.save_subrecipe(form)
+            flash('La nueva subreceta ha sido guardada.', 'success')
+            return redirect(url_for('subrecipes.overview'))
+    return render_template(
+        "edit_subrecipe.html",
+        title="Crear subreceta",
+        subrecipe=subrecipe,
+        recipe_form=SearchRecipeForm(),
+        last_recipes=get_last_recipes(4),
+        all_ingredients=get_all_ingredients(),
+        form=form,
+        is_edit_recipe=True,
+        is_new_subrecipe=True
+    )
+
+
